ffb7ae3df52ce76a74e64bdbe570a0e985cfc8a1
NyusyaR <aasorokina88@gmail.com>
2026-01-08 19:16:27 +0500

    Update main.py

--- recipe_app/main.py
+++ recipe_app/main.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
+from recipe_app.database import create_tables
+from recipe_app.router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):  # контекстный менеджер
+    await create_tables()
+    print("База готова")
+    yield
+    print("Выключение")
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(tasks_router)

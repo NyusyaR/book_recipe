357eaf35eb4356625b25ccfe229f2e8d1389da83
NyusyaR <aasorokina88@gmail.com>
2026-01-08 22:01:02 +0500

    Update router.py

--- recipe_app/router.py
+++ recipe_app/router.py
@@ -5,21 +5,23 @@ from recipe_app.repository import RecipesRepository
 
 
 routers = APIRouter(
-    tags=['Рецепты'],
+    tags=["Рецепты"],
 )
 
 
-@routers.get('/recipes/', response_model=list[RecipeID])
+@routers.get("/recipes/", response_model=list[RecipeID])
 async def get_recipes():
-    """ Получение всех рецептов """
+    """Получение всех рецептов"""
     recipes_orm = await RecipesRepository.find_all()
     return [RecipeID.model_validate(r) for r in recipes_orm]
 
 
-@routers.get('/recipes/{id_recipe}', response_model=RecipeID)
-async def get_one_recipe(id_recipe: int = Path(..., gt=0, description="ID рецепта должен быть больше 0")):
-    """ Получение рецепта по ID
-    Возращает экземпляр RecipeID или None, если рецепт не найден """
+@routers.get("/recipes/{id_recipe}", response_model=RecipeID)
+async def get_one_recipe(
+    id_recipe: int = Path(..., gt=0, description="ID рецепта должен быть больше 0")
+):
+    """Получение рецепта по ID
+    Возращает экземпляр RecipeID или None, если рецепт не найден"""
     recipe = await RecipesRepository.find_one(id_recipe)
     if recipe is None:
         raise HTTPException(status_code=404, detail="Not Found")
@@ -28,10 +30,10 @@ async def get_one_recipe(id_recipe: int = Path(..., gt=0, description="ID рец
     return validated_recipe
 
 
-@routers.post('/recipes/', response_model=RecipeID)
+@routers.post("/recipes/", response_model=RecipeID)
 async def add_recipe(recipe: RecipeAdd):
-    """ Добавление рецепта в БД
-     Возвращает ID добавленного рецепта """
+    """Добавление рецепта в БД
+     Возвращает ID добавленного рецепта"""
     try:
         recipe_add = await RecipesRepository.add_one(recipe)
         return recipe_add

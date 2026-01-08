41b3395a27a083a6607680bc5a90203e39a178ab
NyusyaR <aasorokina88@gmail.com>
2026-01-08 23:10:28 +0500

    Update database.py

--- recipe_app/database.py
+++ recipe_app/database.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlalchemy.ext.asyncio import acreate_async_engine, sync_sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 

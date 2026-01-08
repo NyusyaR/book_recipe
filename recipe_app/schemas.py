5e2a22e672e937ad174941873b4cce6a8f063647
NyusyaR <aasorokina88@gmail.com>
2026-01-08 19:18:09 +0500

    Update schemas.py

--- recipe_app/schemas.py
+++ recipe_app/schemas.py
@@ -1,1 +1,22 @@
+from pydantic import BaseModel, Field
 
+
+class RecipesBase(BaseModel):
+    name: str = Field(description="Название блюда")
+    cooking_time: int = Field(gt=0, description="Время приготовления блюда")
+    ingredients: str = Field(description="Ингридиенты блюда")
+    description: str = Field(description="Описание и этапы приготовления блюда")
+    views_count: int = Field(default=0, description="Количнство просмотров")
+
+
+class RecipeAdd(RecipesBase):
+    """ Что приходит от клиента при создании """
+    pass
+
+
+class RecipeID(RecipesBase):
+    """ Что отдаем клиенту """
+    id_recipe: int = Field(description="Идентификатор рецепта")
+
+    class Config:
+        from_attributes = True

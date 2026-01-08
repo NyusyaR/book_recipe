a154d655145cf3d24255affa458c02d0e08689df
NyusyaR <aasorokina88@gmail.com>
2026-01-08 22:04:16 +0500

    Update repository.py

--- recipe_app/repository.py
+++ recipe_app/repository.py
@@ -11,10 +11,13 @@ from recipe_app.database import new_session, RecipesOrm
 class RecipesRepository:
     @classmethod
     async def find_one(cls, id_recipe) -> Optional[RecipeID]:
-        """ Получение детальной информации по заданному ID рецепта """
+        """Получение детальной информации по заданному ID рецепта"""
         try:
             if id_recipe <= 0 or not isinstance(id_recipe, int):
-                raise HTTPException(status_code=422, detail="Ошибка, id_recipe должен быть целым числом.")
+                raise HTTPException(
+                    status_code=422, 
+                    detail="Ошибка, id_recipe должен быть целым числом."
+                )
             async with new_session() as session:
                 query = select(RecipesOrm).where(RecipesOrm.id_recipe == id_recipe)
                 result = await session.execute(query)
@@ -37,21 +40,23 @@ class RecipesRepository:
 
     @classmethod
     async def find_all(cls) -> Sequence[RecipesOrm]:
-        """ Получение всех рецептов """
+        """Получение всех рецептов"""
         async with new_session() as session:
             try:
                 query = select(RecipesOrm).order_by(
-                    RecipesOrm.views_count.desc(),
-                    RecipesOrm.cooking_time.asc())
+                    RecipesOrm.views_count.desc(), RecipesOrm.cooking_time.asc()
+                )
                 result = await session.execute(query)
                 recipe_model = result.scalars().all()
                 return recipe_model
             except Exception:
-                raise HTTPException(status_code=500, detail="Ошибка сервера при обработке запроса.")
+                raise HTTPException(
+                    status_code=500, detail="Ошибка сервера при обработке запроса."
+                )
 
     @classmethod
     async def find_by_name(cls, name: str) -> Optional[RecipesOrm]:
-        """ Проверка наличия рецепта с указанным названием """
+        """Проверка наличия рецепта с указанным названием"""
         async with new_session() as session:
             query = select(RecipesOrm).filter(RecipesOrm.name == name)
             result = await session.execute(query)
@@ -59,11 +64,14 @@ class RecipesRepository:
 
     @classmethod
     async def add_one(cls, name: RecipeAdd) -> RecipeID:
-        """ Создание нового рецепта """
+        """Создание нового рецепта"""
         async with new_session() as session:
             existing_recipe = await cls.find_by_name(name.name)
             if existing_recipe:
-                raise HTTPException(status_code=500, detail="Рецепт с таким названием уже существует")
+                raise HTTPException(
+                    status_code=500, 
+                    detail="Рецепт с таким названием уже существует"
+                )
 
             recipe_dict = name.model_dump(exclude_unset=True)
             new_recipe = RecipesOrm(**recipe_dict)
@@ -74,7 +82,13 @@ class RecipesRepository:
                 return RecipeID.model_validate(new_recipe)
             except IntegrityError as err:
                 await session.rollback()
-                raise HTTPException(status_code=400, detail=f"Ошибка при создании рецепта: дублирование записей: {err}")
+                raise HTTPException(
+                    status_code=400, 
+                    detail=f"Ошибка при создании рецепта: дублирование записей: {err}"
+                )
             except SQLAlchemyError as err:
                 await session.rollback()
-                raise HTTPException(status_code=500, detail=f"Ошибка при сохранении рецепта: {err}")
+                raise HTTPException(
+                    status_code=500, 
+                    detail=f"Ошибка при сохранении рецепта: {err}"
+                )

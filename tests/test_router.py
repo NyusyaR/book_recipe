ca903344be2aff8b0eee254581aa0642964ed367
NyusyaR <aasorokina88@gmail.com>
2026-01-08 19:24:17 +0500

    Update test_router.py

--- tests/test_router.py
+++ tests/test_router.py
@@ -1,6 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from unittest.mock import AsyncMock
+import httpx
 
 from recipe_app.main import app
 from recipe_app.schemas import RecipeID
